9c51ec44002bc6b1ef2db4b47667c1555d217926
Robert Sheffield <sheffira2@gmail.com>
2023-04-06 17:00:29 -0700

    Deployed backend and updated the prod host

--- src/utils/hosts.js
+++ src/utils/hosts.js
@@ -1,5 +1,5 @@
 let devModeFrontEnd = true;
-let devModeBackend = true;
+let devModeBackend = false;
 
 export const frontendHost = devModeFrontEnd ? 'http://localhost:5173/' : 'http://localhost:5173/';
-export const backendHost = devModeBackend ? 'http://localhost:8080/' : 'http://localhost:8080/';
+export const backendHost = devModeBackend ? 'http://localhost:8080/' : 'https://cookbookapi-production.up.railway.app/';
