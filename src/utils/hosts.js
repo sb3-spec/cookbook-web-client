4ddc5bed56dc9c1c570198e59a86e7f846ed9940
Robert Sheffield <sheffira2@gmail.com>
2023-04-06 17:12:28 -0700

    Tweaking things to get frontend deployed

--- src/utils/hosts.js
+++ src/utils/hosts.js
@@ -1,5 +1,5 @@
-let devModeFrontEnd = true;
+let devModeFrontEnd = false;
 let devModeBackend = false;
 
-export const frontendHost = devModeFrontEnd ? 'http://localhost:5173/' : 'http://localhost:5173/';
+export const frontendHost = devModeFrontEnd ? 'http://localhost:5173/' : 'https://frolicking-longma-9776b7.netlify.app/';
 export const backendHost = devModeBackend ? 'http://localhost:8080/' : 'https://cookbookapi-production.up.railway.app/';
